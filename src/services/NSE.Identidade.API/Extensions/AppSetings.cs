9422f5d4b1904f4babded7ae11cfdb2c868a72fa
Rafael Nunes <rafaelnunes_93@hotmail.com>
2020-12-08 16:03:30 -0300

    Emitindo JWT pela controller

--- src/services/NSE.Identidade.API/Extensions/AppSetings.cs
+++ src/services/NSE.Identidade.API/Extensions/AppSetings.cs
@@ -8,7 +8,7 @@ namespace NSE.Identidade.API.Extensions
     public class AppSettings
     {
         public string Secret { get; set; }
-        public string ExpiracaoHoras { get; set; }
+        public int ExpiracaoHoras { get; set; }
         public string Emissor { get; set; }
         public string ValidoEm { get; set; }
     }
